5c798a4c91b806243c7f8b7ff52d9cfbd7f7b119
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-05-25 22:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let result = []
+ while(arr.length>1){
+     arr.sort()
+     let x = arr[0]+arr[1]
+     result.push(x)
+     arr.splice(0,2)
+     arr.push(x)
+ }
+ let sum = 0
+ result.forEach((ele)=>sum+=ele)
+return sum
   
 }
 
