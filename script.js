20c70ea6fa4da9292b57c5c264f95a395810eed3
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-05-25 22:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	let result = 0
- while(arr.length>1){
-     arr.sort()
-     let x = arr[0]+arr[1]
-     result +=x
-     arr.splice(0,2)
-     arr.push(x)
- }
-return result
-  
+function mincost(arr) {
+  let result = 0;
+  arr.sort((a, b) => a - b); // sort array in ascending order at the beginning
+  while(arr.length > 1) {
+    let sum = arr[0] + arr[1];
+    result += sum;
+    arr.splice(0, 2); // remove the first two elements
+    arr.push(sum); // add the sum to the array
+    arr.sort((a, b) => a - b); // sort array again after adding new element
+  }
+  return result;
 }
 
 module.exports=mincost;
